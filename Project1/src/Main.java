3beff0d35cdfbeb8f23f1c96b2f2dd54e0cc97c1
Hritik Mahajan <hritik.mahajan@gmail.com>
2025-08-18 15:02:35 +0530

    Data types

--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -1,7 +1,23 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello Buorld");
-        System.out.println("Goodbye Buorld");
+        int age = 35;
+        int year = 2024;
+        boolean isStudent = true;
+        double CGPA = 9.2;
+        String name = "Student1";
+
+        if(isStudent)
+        {
+            System.out.println("My name is " + name);
+            System.out.println("I am " + age + " years old");
+            System.out.println("Your CGPA is " + CGPA);
+            System.out.println("The year is " + year);
+        }
+        else
+        {
+            System.out.println("No data available");
+        }
+
     }
 }
