209a2e25ca2fd1042023c07e8394a28cf0459c16
Hritik Mahajan <hritik.mahajan@gmail.com>
2025-09-15 12:25:22 +0530

    Area program

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        // Calculate the area of a rectangle
+
+        double width = 0.0;
+        double height = 0.0;
+        double area = 0.0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter width: ");
+        width = scanner.nextDouble();
+        scanner.nextLine(); // Clear input buffer
+
+        System.out.print("Enter height: ");
+        height = scanner.nextDouble();
+        scanner.nextLine(); // Clear input buffer
+
+        area = width * height;
+        System.out.println("Area of the rectangle is: " + area + " cm");
+
+        scanner.close();
+    }
 }
